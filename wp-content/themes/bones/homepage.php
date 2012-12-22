6e04b278a97503ada06295f1def964f6c9553645
toretto460 <toretto460@gmail.com>
2012-12-22 17:45:44 +0100

    Centered posts in home

--- wp-content/themes/bones/homepage.php
+++ wp-content/themes/bones/homepage.php
@@ -10,7 +10,7 @@ Template Name: Homepage
 			
 				<div id="inner-content" class="wrap clearfix">
 			
-				    <div id="main" class="eightcol first clearfix" role="main">
+				    <div id="main" class="tencol centered clearfix" role="main">
 
 							<?php
 								global $post;
