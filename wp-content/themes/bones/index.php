c83cdadff119a6fffe85f35d44df81917d76228d
Toretto460 <toretto460@gmail.com>
2013-03-13 17:06:07 +0100

    Adde permalink to post in homepage

--- wp-content/themes/bones/index.php
+++ wp-content/themes/bones/index.php
@@ -24,7 +24,7 @@ Template Name: Homepage
 						
 						    <header class="article-header">
 							
-							    <h1 class="page-title"><?php the_title(); ?></h1>
+							    <h1 class="page-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h1>
 							    
                   <p class="byline vcard"><?php
                     //printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link());
