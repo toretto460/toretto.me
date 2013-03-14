ffce7d40e31fe4dd781dc2a3006dc7d18b1eb82c
Toretto460 <toretto460@gmail.com>
2013-03-14 16:55:43 +0100

    Fixed post view

--- wp-content/themes/bones/single.php
+++ wp-content/themes/bones/single.php
@@ -28,7 +28,7 @@
 							
 								</footer> <!-- end article footer -->
 					
-								<?php comments_template(); ?>
+								<?php // Removed comments block from post view comments_template(); ?>
 					
 							</article> <!-- end article -->
 					
@@ -38,7 +38,7 @@
 					
 							<article id="post-not-found" class="hentry clearfix">
 					    		<header class="article-header">
-					    			<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
+					    			<h1><?php _e("Oops, Post Not Found!", "	"); ?></h1>
 					    		</header>
 					    		<section class="entry-content">
 					    			<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
