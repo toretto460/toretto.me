6029b0ea1211fe8bc7e4e4fe1bfced138d52bf25
Toretto460 <toretto460@gmail.com>
2013-04-19 11:47:19 +0200

    Added analytics js code in header

--- wp-content/themes/bones/header.php
+++ wp-content/themes/bones/header.php
@@ -33,6 +33,17 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-40264682-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	
 		<div id="container">
 			
